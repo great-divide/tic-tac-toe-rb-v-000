d48031d7c36e8dfb2497b99dd680132d1abf8aba
Brian Birdwell <bbirdwell@gmail.com>
2018-09-28 18:01:14 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -7,12 +7,26 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+
+def input_to_index(user_input) 
+  return user_input.to_i - 1
+end
+
+def move(board, position, character = "X")
+  board[position] = character
+end
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
-
-# Define your WIN_COMBINATIONS constant
 
 WIN_COMBINATIONS = [ 
   [0, 1, 2],
