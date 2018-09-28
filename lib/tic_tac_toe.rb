907fc2d852bbff3922e86601e00506835da3140b
Brian Birdwell <bbirdwell@gmail.com>
2018-09-28 18:02:53 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -110,3 +110,20 @@ def current_player(board)
   else return "O"
   end
 end
+
+def valid_move?(board, index)
+  if (position_taken?(board, index) == false) && (index.between?(0, 8))
+     return true 
+  elsif position_taken?(board, index) == true
+    return false
+  end
+end
+
+
+def position_taken?(board, index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else 
+    return true
+  end
+end
