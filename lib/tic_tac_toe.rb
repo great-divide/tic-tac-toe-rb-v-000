037679f230950c6bd8d127ab465f2cf230209524
Brian Birdwell <bbirdwell@gmail.com>
2018-09-28 18:49:20 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,13 +12,10 @@ def input_to_index(user_input)
   return user_input.to_i - 1
 end
 
-def move(board, position, character)
-  board[position] = character
+def move(board, index, current_player(board))
+  board[index] = current_player(board)
 end
 
-def character
-  character = current_player(board)
-end
 
 def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
